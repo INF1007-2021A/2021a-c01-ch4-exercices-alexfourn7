f1e0b81c0725856c56bb3d3c29b07cdb5427e077
alexfourn7 <90001193+alexfourn7@users.noreply.github.com>
2021-09-14 17:21:35 -0400

    exercices compléter

--- exercice.py
+++ exercice.py
@@ -3,23 +3,47 @@
 
 
 def is_even_len(string: str) -> bool:
-    pass
+    return len(string) % 2 == 0
+
 
 
 def remove_third_char(string: str) -> str:
-    pass
+    string2 = ""
+    index = 0
+    for lettre in string:
+        if index != 2:
+            string2 += lettre
+        index += 1
+    return string2
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    pass
+    string2 = ""
+    for letter in string:
+        if letter == old_char:
+            string2 += new_char
+        else:
+          string2 += letter
+    return string2
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    pass
+    counter = 0
+    for letter in string:
+        if letter == char:
+            counter += 1
+    return counter
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    pass
+    counter = 0
+    for w in sentence.split():
+       if w == word:
+           counter += 1
+
+    return counter
+
+
 
 
 def main() -> None:
